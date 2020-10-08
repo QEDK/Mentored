0b62144a3c2a723af7a5e259ae95f5b261fdb1ce
QEDK <1994constant@gmail.com>
2020-10-08 13:07:12 +0530

    backend: Add signup and signin routes

--- backend/mentored/urls.py
+++ backend/mentored/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from CuratedLists import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/signin', views.signin, name='signin'),
+    path('api/signin/', views.signin, name='signin'),
+    path('api/signup', views.signup, name='signup'),
+    path('api/signup/', views.signup, name='signup'),
 ]
