d264a270f5f2d7ce6b4af85c3e9e6999d1d9cd7e
Yash Khare <yashsja@gmail.com>
2020-10-08 23:42:34 +0530

    backend: Make changes for local testing

--- backend/mentored/settings.py
+++ backend/mentored/settings.py
@@ -88,6 +88,8 @@ DATABASES = {
         }
     }
 
+DATABASES['default']['HOST'] = '127.0.0.1'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
