88f413424d6390014a7d05fe325bc2eef984478a
QEDK <1994constant@gmail.com>
2020-10-11 11:21:20 +0530

    backend: Add wildcard CORS headers

---
+++ backend/mentored/corsMiddleware.py
@@ -0,0 +1,7 @@
+def cors_middleware(get_response):
+    def middleware(request):
+        response = get_response(request)
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Headers"] = "*"
+        return response
+    return middleware
--- backend/mentored/settings.py
+++ backend/mentored/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mentored.corsMiddleware.cors_middleware',
 ]
 
 ROOT_URLCONF = 'mentored.urls'
