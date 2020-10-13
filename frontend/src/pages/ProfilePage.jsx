d5033345a63ca1f08ddd40916f576a5c79789359
Preet Shah <shahpreetk@gmail.com>
2020-10-13 15:42:57 +0530

    frontend: profile page display profile info

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import axios from 'axios'
@@ -7,12 +7,14 @@ class ProfilePage extends Component {
     constructor() {
         super();
         this.state = {
-            profile: '',
-            username: ''
+            profile: {},
+            username: '',
+            fields: {},
+            curations: {}
         };
     }
 
-    componentDidMount(username, profile) {
+    componentDidMount(username, profile, fields, curations) {
         const user = localStorage.getItem('username')
         const send = {
             username: user
@@ -20,59 +22,53 @@ class ProfilePage extends Component {
         console.log('username =', username)
         axios.post('https://mentored-n3wkrveexq-uc.a.run.app/api/get_profile', send)
             .then((res) => {
-                profile = res.data
+                profile = res.data[0]
+                fields = profile.fields
+                curations = profile.curations
                 this.setState({
-                    profile: profile
+                    profile: profile,
+                    fields: fields,
+                    curations: curations
                 })
                 console.log('profile = ', profile)
+                console.log('fields = ', fields)
+                console.log('curations = ', curations)
             })
             .catch(err => {
                 console.error(err)
             })
     }
+    render() {
+        const { profile, fields, curations } = this.state;
+        return (
+            <Row>
+                        <Col md={3}>
+                            <h2>User Profile</h2>
+                            <Form>
+                                <Form.Group controlId='name'>
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Control disabled type='name' placeholder={fields.name}></Form.Control>
+                                </Form.Group>
+                                <Form.Group controlId='username'>
+                                    <Form.Label>Username</Form.Label>
+                                    <Form.Control disabled name='username' type='text' placeholder={fields.username}>
+                                    </Form.Control>
+                                </Form.Group>
 
-    // axios.post('https://mentored-n3wkrveexq-uc.a.run.app/api/get_profile', profile)
-    //     .then(res => {
-    //         setProfile(res)
-    //         console.log(res)
-    //     })
-    //     .catch(err=> {
-    //         console.error(err)
-    //     })
-    render(){
-        // console.log('rendered mentor profile : ', this.state.profile)
-        const { profile } = this.state;
-    return (
-        <Row>
-            <Col md={3}>
-                <h2>User Profile</h2>
-                <Form>
-                    <Form.Group controlId='name'>
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control disabled type='name' placeholder='Preet'></Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId='username'>
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control disabled name='username' type='text' placeholder='tryingSignup2'>
-                        </Form.Control>
-                    </Form.Group>
-                    {/* <Form.Group controlId='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type='password' placeholder='Enter Password'></Form.Control>
-                    </Form.Group> */}
-                    <Form.Group controlId='company'>
-                        <Form.Label>Company</Form.Label>
-                        <Form.Control disabled type='text' name='company' placeholder='Mentored'></Form.Control>
-                    </Form.Group>
-                    {/* <Button type='submit' variant='primary'>Update</Button> */}
-                </Form>
-            </Col>
-            <Col className='profilecur pl-5' md={9}>
-                <h2>Your Curated Lists</h2>
-                <Link to='/profile/makelist'><Button>Create New</Button></Link>
-            </Col>
-        </Row>
-    )}
+                                <Form.Group controlId='company'>
+                                    <Form.Label>Company</Form.Label>
+                                    <Form.Control disabled type='text' name='company' placeholder={fields.company}></Form.Control>
+                                </Form.Group>
+
+                            </Form>
+                        </Col>
+                        <Col className='profilecur pl-5' md={9}>
+                            <h2>Your Curated Lists</h2>
+                            <Link to='/profile/makelist'><Button>Create New</Button></Link>
+                        </Col>
+            </Row>
+        )
+    }
 }
 
 export default ProfilePage
